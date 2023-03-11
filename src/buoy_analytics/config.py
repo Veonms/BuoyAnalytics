f191bbc44b391298ed321cae603d40641ef967d2
Veonms <rwgalloway00@gmail.com>
2023-03-11 19:59:59 +0000

    - Added functionality to retrieve buoy data. - Added a retry decorator for retrieving data.

---
+++ src/buoy_analytics/config.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+RAW_BUOY_DATA_URL: str = "https://www.ndbc.noaa.gov/data/latest_obs/latest_obs.txt"
--- src/buoy_analytics/main.py
+++ src/buoy_analytics/main.py
@@ -1,5 +1,21 @@
+import logging
+
+from buoy_analytics.config import RAW_BUOY_DATA_URL
+from buoy_analytics.utils.exceptions import NoDataRetrieved
+from buoy_analytics.utils.ndbc_data import retrieve_buoy_data
+
+
 def main():
-    return 1
+    logging.info("Retrieving active buoys")
+
+    try:
+        raw_buoys = retrieve_buoy_data(url=RAW_BUOY_DATA_URL)
+    except NoDataRetrieved as err:
+        logging.error(f"An error has occured: {err}")
+        exit()
+
+    logging.info("Active buoys retrieved")
+    print(raw_buoys)
 
 
 if __name__ == "__main__":
